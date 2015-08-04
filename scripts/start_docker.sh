0ef12d4e7dde8284d0d4c9ecb4ed10384d67f7c1
Xabier de Zuazo <xabier@zuazo.org>
2015-08-04 07:14:22 +0200

    Ensure that the eth0 interface is up inside UML

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -71,7 +71,9 @@ travis_fold start docker.start
   travis_section 'Starting Docker Engine'
   sekexe/run \
                'echo 2000 2500 > /proc/sys/net/ipv4/ip_local_port_range ' \
-               '&& /sbin/ifconfig -a' \
+               '&& ifconfig -a' \
+               '&& ifconfig eth0 up' \
+               '&& ifconfig -a' \
                '&& ( sleep 5 ' \
                     '&& ps axu ' \
                     '&& netstat -puatn & ) ' \
