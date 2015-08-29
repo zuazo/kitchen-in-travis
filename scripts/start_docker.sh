934f26f6c30adcc580f664a40c9aac37c37fc352
Xabier de Zuazo <xabier@zuazo.org>
2015-08-29 08:18:47 +0200

    Use curl instead of wget (fix gpg: no valid OpenPGP data found)

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -14,8 +14,13 @@ DOCKER_PORT_RANGE=2400:2500
 SLIRP_PORTS=$(seq 2000 2500)
 export HOST_IP DOCKER_HOST DOCKER_PORT_RANGE SLIRP_PORTS
 
+echo 'Installing cURL'
+sudo apt-get -y update
+sudo apt-get install -y ca-certificates curl
+echo ''
+
 echo 'Installing docker repository'
-wget -qO- https://get.docker.io/gpg | sudo apt-key add -
+curl https://get.docker.com/gpg | sudo apt-key add -
 echo 'deb https://get.docker.io/ubuntu docker main' | sudo tee /etc/apt/sources.list.d/docker.list
 echo ''
 
